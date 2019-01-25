e749fedf608c3bb7973d226cffa30ecc2798c4a0
Love Hermansson <love.hermansson@visma.com>
2019-01-25 14:40:34 +0100

    Love dev

--- ClientApp/components/type-fast.vue
+++ ClientApp/components/type-fast.vue
@@ -1,27 +1,96 @@
 <template>
-  <div class="typeFast__container">
-      <h1>Type fast</h1>
-      <hr />
+  <div class="typeFast__container--marginRight">
+    <h1>Type fast</h1>
+    <hr />
+    <div class="row">
+      <div class="col-4">
+        <p class="typeFast__gameInfo">Tries left: {{numberOfTries}}</p>
+      </div>
+      <div class="col-4">
+        <p class="typeFast__gameInfo">Score: {{score}}</p>
+      </div>
+      <div class="col-4">
+        <p class="typeFast__gameInfo">Level: {{level}}</p>
+      </div>
+    </div>
+
+    <div class="typeFast__gameArea">
+      <div class="typeFast__slideArea">
+        <p class="typeFast__word">{{word}}</p>
+      </div>     
+    </div>
+    <input type="text" autofocus class="typeFast__textInput" v-model="inputText" @input="onTextInput"><br>
   </div>
 </template>
 
 <script>
+  import JQuery from 'jquery';
+  let $ = JQuery;
 
   export default {
     data: () => ({
+      score: 0,
+      numberOfTries: 5,
+      level: 1,
+      word: "",
+      inputText: "",
     }),
 
     mounted() {
+      this.init();
     },
 
     methods: {
+
+      init() {
+        this.generateWord();
+        this.slideWord();
+      },
+
+      generateWord() {
+        let text = "";
+        let possible = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        for (let i = 0; i < 3; i++) {
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        this.word = text;
+      },
+
+      slideWord() {
+        let $root = this;
+        $('.typeFast__slideArea').animate({ 'margin-left': '100%' }, 10000, function () {
+          $root.timedOut();
+        });
+      },
+
+      timedOut() {
+        if (this.numberOfTries != 0) {
+          this.numberOfTries--;
+          this.resetWord();
+        }
+      },
+
+      onTextInput() {
+        if (this.inputText === this.word) {
+          this.score += (this.word.length * 10);
+          $('.typeFast__slideArea').stop();
+          this.resetWord();
+        }
+      },
+
+      resetWord() {
+        this.word = "";
+        this.generateWord();
+        $('.typeFast__slideArea').css('margin-left', '-10%');
+        this.inputText = "";
+        this.slideWord();
+      }
     }
   };
 
 </script>
 
-<style scoped>
-  .typeFast__container {
-    text-align: center;
-  }
+<style lang="scss">
+  @import 'type-fast.scss';
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,15 @@ module.exports = () => {
         { test: /\.vue$/, include: /ClientApp/, use: 'vue-loader' },
         { test: /\.js$/, include: /ClientApp/, use: 'babel-loader' },
         { test: /\.css$/, use: isDevBuild ? ['style-loader', 'css-loader'] : ExtractTextPlugin.extract({ use: 'css-loader' }) },
-        { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+        { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' },
+        {
+          test: /\.scss$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        }
       ]
     },
     plugins: [
